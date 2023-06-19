84c39139c691acf7c99c647d996340e196c9e13d
Daryl del Rosario <daryldelrosario.is@gmail.com>
2023-06-19 12:27:29 -0400

    attempt to connect firebase

--- js/main.js
+++ js/main.js
@@ -1,3 +1,38 @@
+// FIREBASE CONNECTION
+const firebaseConfig = {
+    apiKey: "AIzaSyDtQaS74vVjO5rjdlEvzyOaVBoS7Dg_oA4",
+    authDomain: "fir-frontend-2f73b.firebaseapp.com",
+    databaseURL: "https://fir-frontend-2f73b-default-rtdb.firebaseio.com",
+    projectId: "fir-frontend-2f73b",
+    storageBucket: "fir-frontend-2f73b.appspot.com",
+    messagingSenderId: "78300276106",
+    appId: "1:78300276106:web:5c5ec43f2614cb2a181473"
+  };
+
+// initialise firebase
+firebase.initializeApp(firebaseConfig);
+
+// reference your database
+let contactFormDB = firebase.database().ref('contact-form-html-firebase');
+
+document.getElementById('contact-form').addEventListener("submit", submitForm);
+
+function submitForm(e) {
+    e.preventDefault();
+
+    let name = getElementVal("name");
+    let email = getElementVal("email");
+    let message = getElementVal("msg-content");
+
+    console.log(name, email, message);
+}
+
+const getElementVal = (id) => {
+    return document.getElementById(id).value;
+}
+
+// REGULAR
+
 const menuLinks = document.querySelectorAll("#hamburger-nav .menu-links a");
 const hamburgerIcon = document.querySelector(".hamburger-icon");
 
@@ -18,4 +53,3 @@ menuLinks.forEach(function(link) {
 hamburgerIcon.addEventListener("click", toggleMenu);
 
 yearSpan.textContent = thisYear;
-
